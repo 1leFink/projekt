0b440ff6ad904ab2e26c52322aa534a58fc9d231
Leon Fink <Leon.fink.lf@gmail.com>
2021-12-22 15:24:15 +0100

    -

--- src/hft/projekt/Projekt.java
+++ src/hft/projekt/Projekt.java
@@ -18,7 +18,7 @@ public class Projekt{
 		if(opened == false) {
 			System.out.println(Misc.BANNER.getWert());
 			opened = true;
-			System.out.println("Für eine liste der Befehle: help");
+			System.out.println("Für eine liste der Befehle 'help' eingeben. Um das programm zu beenden 'quit' eingeben.");
 		}
 		
 		Kundenverwaltung verwaltung;
